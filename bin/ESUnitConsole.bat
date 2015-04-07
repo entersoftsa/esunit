0f24c22d4817193d36d6cf4c9a4e8a4343a18a26
vpa-es <vpa@entersoft.gr>
2015-04-07 11:48:11 +0300

    added bin folder

---
+++ bin/ESUnitConsole.bat
@@ -0,0 +1,2 @@
+esunitconsole /srv vpa-pc4\sql2012 /auth /db ESTestCases /dbpath e:\Databases\Data\ /bu e:\Databases\Backup\ESTestCases /essrv LOCAL /esport 8032 /esdb ESTestCases /esuid admin /espwd entersoft /escompany 001 /esbranch 1 /frommail vpa@entersoft.gr /tomail vpa@entersoft.gr /list "d8fe820c-bf08-4d6c-9f0c-bd6b692b3c2c"
+
---
+++ bin/ESUnitConsoleExport.bat
@@ -0,0 +1,3 @@
+rem esunitconsole /debug /srv vpa-pc4\sql2012 /auth /db ESTestCases /dbpath e:\Databases\Data\ /bu e:\Databases\Backup\ESTestCases /essrv LOCAL /esport 8091 /esdb ESTestCases /esuid admin /espwd entersoft /escompany 001 /esbranch 1 /frommail vpa@entersoft.gr /tomail vpa@entersoft.gr /list "e6c8a53c-fac9-401e-9756-144bd43001c0"
+
+esunitconsole /debug /essrv LOCAL /esport 8091 /esdb ESTestCases /esuid admin /espwd entersoft /escompany 001 /esbranch 1 /export
